c2ee296e7036e3dba946e29e84cb5456e9671514
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-11-17 19:22:28 -0600

    initial commit

---
+++ levelupapi/models/__init__.py
@@ -0,0 +1,4 @@
+from .game import Game
+from .event import Event
+from .gamer import Gamer
+from .game_type import GameType
---
+++ levelupapi/models/game_type.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+
+class GameType(models.Model):
+    label = models.CharField(max_length=25)
