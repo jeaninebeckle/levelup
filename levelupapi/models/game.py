3df5ca27948ed8d6538c4d0bcfdfbb61a063278c
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-01-30 09:53:27 -0600

    html displays in browser

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -5,10 +5,6 @@ class Game(models.Model):
     title = models.CharField(max_length=75)
     maker = models.CharField(max_length=50)
     skill_level = models.IntegerField()
-    gamer = models.ForeignKey("Gamer",
-    on_delete=CASCADE, related_name="games",
-    related_query_name="game")
-    gametype = models.ForeignKey("GameType",
-    on_delete=CASCADE, related_name="games",
-    related_query_name="game")
+    gamer = models.ForeignKey("Gamer", on_delete=CASCADE, related_name="games", related_query_name="game")
+    gametype = models.ForeignKey("GameType", on_delete=CASCADE, related_name="games",related_query_name="game")
     number_of_players = models.IntegerField()
