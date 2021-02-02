0e2aa0a9aad07a0e12dc896bc4378077b2a9f1ec
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-02-01 18:28:46 -0600

    refactor to use views

--- levelupreports/views/users/eventsbyuser.py
+++ levelupreports/views/users/eventsbyuser.py
@@ -16,23 +16,15 @@ def userevent_list(request):
             # Query for all events, with related user info.
             db_cursor.execute("""
                 SELECT
-                    e.id,
-                    e.description,
-                    e.date,
-                    e.time,
-                    g.title,
-                    u.id user_id,
-                    u.first_name || ' ' || u.last_name AS full_name
+                    id,
+                    description,
+                    date,
+                    time,
+                    title,
+                    user_id,
+                    full_name
                 FROM
-                    levelupapi_event e
-                JOIN
-                    levelupapi_gamerevent ge ON e.id = ge.event_id
-                JOIN
-                    levelupapi_gamer gr ON ge.gamer_id = gr.id
-                JOIN
-                    levelupapi_game g ON e.game_id = g.id
-                JOIN
-                    auth_user u ON gr.user_id = u.id
+                    EVENTS_BY_USER
             """)
 
             dataset = db_cursor.fetchall()
